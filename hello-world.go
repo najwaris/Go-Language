18651cd0d50c7984e7021e7516497785a77d7ff3
NURUL NAJWA BINTI MAT ARIS <122419185+najwaris@users.noreply.github.com>
2024-02-01 23:41:39 +0800

    Create hello-world.go

---
+++ hello-world.go
@@ -0,0 +1,10 @@
+// hello-world.go
+package main
+
+// Import the fmt package to print to the console
+import "fmt"
+
+func main() {
+	// Print "hello world" to the console
+	fmt.Println("hello world")
+}
