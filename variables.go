355d3c1471eb29dd35df606ba4d84d9e183e7656
NURUL NAJWA BINTI MAT ARIS <122419185+najwaris@users.noreply.github.com>
2024-02-07 12:09:06 +0800

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // Initializing variable 'a' with the value "initial"
+    var a = "initial" 
+    fmt.Println(a)
+
+    // Initializing variables 'b' and 'c' with the values 1 and 2 respectively
+    var b, c int = 1, 2 
+    fmt.Println(b, c)
+
+    // Initializing variable 'd' with the value true
+    var d = true 
+    fmt.Println(d)
+
+    // Declaring variable 'e' without initializing it
+    var e int 
+    fmt.Println(e)
+
+    // Short variable declaration, initializing variable 'f' with the value "apple"
+    f := "apple" 
+    fmt.Println(f)
+}
