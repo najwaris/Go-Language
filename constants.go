1089784fd40a1d49b2e93cbd4dc475178ee0c0da
NURUL NAJWA MAT ARIS <122419185+najwaris@users.noreply.github.com>
2024-02-08 21:01:42 +0800

    Create constants.go

---
+++ constants.go
@@ -0,0 +1,31 @@
+package main
+
+// Import fmt and math
+import (
+    "fmt"
+    "math"
+)
+
+// This is a constant string declaration
+const s string = "constant" 
+
+func main() {
+  
+    // Print the value of the constant string
+    fmt.Println(s) 
+
+    // Declare a constant integer
+    const n = 500000000 
+
+    // Calculate the value of a constant floating-point number
+    const d = 3e20 / n 
+
+    // Print the value of the constant floating-point number
+    fmt.Println(d) 
+
+    // Convert the constant floating-point number to an int64 and print it
+    fmt.Println(int64(d)) 
+
+    // Calculate and print the sine of the constant integer
+    fmt.Println(math.Sin(n)) 
+}
